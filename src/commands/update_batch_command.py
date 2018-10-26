c76eb47c7adab29774019909be333ee412467075
Adriano Gil <adrianogil.san@gmail.com>
2018-10-25 21:09:14 -0400

    Update in batch using conditions

--- src/commands/update_batch_command.py
+++ src/commands/update_batch_command.py
@@ -1,10 +1,6 @@
 def execute(args, extra_args, controller):
-    # if len(args) == 0:
-    #     category='%'
-    # elif len(args) > 0:
-    #     category = args[0]
-
-    repo_list = controller.get_repos()
+    search_conditions = controller.get_search_conditions(args, extra_args)
+    repo_list = controller.get_repos(search_conditions)
     for repo in repo_list:
         print("###################################################")
         print('Repo ' + str(repo.id) + ': Updating ' + repo.name)
