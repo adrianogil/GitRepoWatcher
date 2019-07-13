c582ae11ed24f2333ca0468c25db13d77a662693
Adriano Gil <adrianogil.san@gmail.com>
2019-07-13 10:26:42 -0400

    Added WIP notification for new commits

--- src/commands/update_batch_command.py
+++ src/commands/update_batch_command.py
@@ -1,7 +1,29 @@
+import platform
+import subprocess
+
+
 def execute(args, extra_args, controller):
     search_conditions = controller.get_search_conditions(args, extra_args)
     repo_list = controller.get_repos(search_conditions)
     for repo in repo_list:
         print("###################################################")
         print('Repo ' + str(repo.id) + ': Updating ' + repo.name)
-        controller.update_gitrepo(repo)
+        new_commits = controller.update_gitrepo(repo)
+        new_commits = int(new_commits)
+        if new_commits > 0:
+            notify_new_commits(repo.name, new_commits)
+
+
+def is_termux():
+    return platform.machine() == "aarch64"
+
+
+def notify_new_commits(repo_name, new_commits):
+    if is_termux():
+        pass
+    else:
+        subprocess_cmd = 'terminal-notifier -title "Repo %s" -message "%s new commits!"' % (
+                repo_name,
+                new_commits
+            )
+        subprocess.check_output(subprocess_cmd, shell=True)
