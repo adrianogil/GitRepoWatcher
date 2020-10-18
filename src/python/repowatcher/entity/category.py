83ae364448c370e87d46e27b5495241e1be043dd
Adriano Gil <adrianogil.san@gmail.com>
2020-10-18 17:57:14 -0400

    Added property generation to category entity

--- src/python/repowatcher/entity/category.py
+++ src/python/repowatcher/entity/category.py
@@ -1,4 +1,17 @@
+from pyutils.code.classproperties import declare_props
+
 class Category:
-    def __init__(self, name):
-        self.id = -1
-        self.name = name
+    def __init__(self, name=''):
+        declare_props(
+            self,
+            props={
+                'id': {
+                    'prop_type': int,
+                    'default_value': -1
+                },
+                'name': {
+                    'prop_type': str,
+                    'default_value': name
+                }
+            }
+        )
