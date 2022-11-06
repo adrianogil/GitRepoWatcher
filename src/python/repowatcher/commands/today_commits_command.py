912d62a7de51465315ebaed28e8ef3cbf65b39ae
Adriano Gil <adrianogil.san@gmail.com>
2022-11-05 20:31:53 -0400

    Handle repo with same name but different path when checking today commits

--- src/python/repowatcher/commands/today_commits_command.py
+++ src/python/repowatcher/commands/today_commits_command.py
@@ -30,9 +30,10 @@ def execute(args, extra_args, controller):
                 total_repos_with_new_commits += 1
 
             if total_today_commits > 0:
-                report_data[repo.name] = {}
-                report_data[repo.name]['repo-id'] = repo.id
-                report_data[repo.name]['repo-path'] = repo.path
+                report_data[repo.path] = {}
+                report_data[repo.path]['repo-id'] = repo.id
+                report_data[repo.path]['repo-name'] = repo.name
+                report_data[repo.path]['repo-path'] = repo.path
                 total_commits_in_all_repos = total_commits_in_all_repos + total_today_commits
                 commits = []
                 for c in today_commits_msgs:
@@ -42,7 +43,7 @@ def execute(args, extra_args, controller):
                     commit_data['commit-msg'] = c[len(c.split(' ')[0]):].strip()
 
                     commits.append(commit_data)
-                report_data[repo.name]['commits'] = commits
+                report_data[repo.path]['commits'] = commits
         except Exception as error:
             print(error)
 
@@ -53,10 +54,11 @@ def execute(args, extra_args, controller):
         with open(extra_args['--json'][0], 'w') as f:
             json.dump(report_data, f)
     else:
+        total_commits_in_all_repos = 0
         for r in report_data:
             print("###################################################")
-            print('Repo %s (Id %s) ' % (r, report_data[r]['repo-id']))
-            total_commits_in_all_repos = total_commits_in_all_repos + total_today_commits
+            print('Repo %s (Id %s) ' % (report_data[r]['repo-name'], report_data[r]['repo-id']))
+            total_commits_in_all_repos = total_commits_in_all_repos + len(report_data[r]['commits'])
             for c in report_data[r]['commits']:
                 print(c['commit-id'] + " " + c['commit-msg'])
         print("###################################################")
